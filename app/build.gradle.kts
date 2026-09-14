88e58ace84644cc3b7f333e015b4970e63473724
Captain-Vikram <vighneshkontham@gmail.com>
2026-09-14 17:42:43 +0530

    fix(release): disable R8 minification and resource shrinking for 100% debug parity in release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -161,10 +161,10 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile("proguard-android.txt"),
                 "proguard-rules.pro"
             )
         }
